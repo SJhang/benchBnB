84974807f4823597add1c2eb0f9aa923601e632f
Sonik Jhang <sonik.jhang@utexas.edu>
2016-10-28 16:56:54 -0700

    root middleware setup

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,13 +1,13 @@
 import { createStore } from 'redux';
 import RootReducer from '../reducers/root_reducer';
-import SessionMiddleware from '../middleware/session_middleware';
+import RootMiddleware from '../middleware/root_middleware';
 
 console.log(RootReducer());
 const configureStore = (preloadedState = {}) => (
   createStore(
     RootReducer,
     preloadedState,
-    SessionMiddleware
+    RootMiddleware
   )
 );
 
