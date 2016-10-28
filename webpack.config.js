a3398c8cc1d197a048e46718d7de6fb55c539c36
Sonik Jhang <sonik.jhang@utexas.edu>
2016-10-28 14:39:53 -0700

    user model setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
   context: __dirname,
-  entry: './frontend/pokedex.jsx',
+  entry: './frontend/benchBnB.jsx',
   output: {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
