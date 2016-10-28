e29007832967139207b51548c12136e137335a33
Sonik Jhang <sonik.jhang@utexas.edu>
2016-10-28 14:54:07 -0700

    live react

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,19 @@
 class Api::SessionsController < ApplicationController
   def create
+    @user = User.find_by_credentials(user_params[:username], user_params[:password])
+    if @user
+      login(@user)
+      render :show
+    else
+      render json: "Invalid username/password", status: 402
+    end
   end
 
   def destroy
+    logout
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,19 @@
 class Api::UsersController < ApplicationController
   def create
+    @user = User.create(user_params)
+    if @user.save
+      login(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
