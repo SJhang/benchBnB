1dd04ea066b58d65d425b6c374c03fddc86d92f7
Sonik Jhang <sonik.jhang@utexas.edu>
2016-10-29 22:42:21 -0700

    frontend

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
 
   let store = configureStore();
+  ReactDOM.render(<Root store={store} />, document.getElementById('root'));
+
   window.store = store;
-  ReactDOM.render(<h1>React live</h1>, document.getElementById('root'));
 });
